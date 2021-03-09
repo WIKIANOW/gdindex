118445bfe554aad7e192f406ba889b734a773353
WIKIANOW <80196812+WIKIANOW@users.noreply.github.com>
2021-03-09 12:38:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5510,10 +5510,10 @@ document.write(
   '<script src="//cdn.jsdelivr.net/gh/WIKIANOW/gdindex@a1cbaac2e06a22fe3a93978223f552c8e60c4a57/js/flv.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/WIKIANOW/gdindex@a1cbaac2e06a22fe3a93978223f552c8e60c4a57/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/WIKIANOW/gdindex@f53e09853df2b68b30d931055bcf9c4b60e01c24/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/WIKIANOW/gdindex@a1cbaac2e06a22fe3a93978223f552c8e60c4a57/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/WIKIANOW/gdindex@f53e09853df2b68b30d931055bcf9c4b60e01c24/js/markdown-it.min.js"></script>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
